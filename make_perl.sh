aa0cecbd9f631fad0eb69ca52e4e3ecb0faf4910
gastonlp <13681487+gastonlp@users.noreply.github.com>
2025-03-18 22:08:17 -0400

    Update make_perl.sh
    fixed perl lib path

--- make_perl.sh
+++ make_perl.sh
@@ -1,1 +1,1 @@
-cd ./modules; perl Makefile.PL lib=./perl; make install; perl Makefile.PL lib=/usr/share/perl/5.34/lib/perl5; make install;
+cd ./modules; perl Makefile.PL lib=/usr/local/lib/x86_64-linux-gnu/perl/5.34.0; make install; perl Makefile.PL lib=/usr/local/lib/x86_64-linux-gnu/perl/5.34.0; make install;
